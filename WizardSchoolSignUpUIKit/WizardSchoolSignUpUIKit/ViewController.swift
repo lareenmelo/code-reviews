18cb641f83a02614dff3aee61e66188c32390f13
lareenmelo <lareenmmelo@gmail.com>
2021-01-03 11:40:40 -0500

    Add UI elements and initial code

--- WizardSchoolSignUpUIKit/WizardSchoolSignUpUIKit/ViewController.swift
+++ WizardSchoolSignUpUIKit/WizardSchoolSignUpUIKit/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private var userNameTextField: UITextField!
+    @IBOutlet private var userNameIcon: UIImageView!
+    @IBOutlet private var passwordTextField: UITextField!
+    @IBOutlet private var passwordIcon: UIImageView!
+    @IBOutlet private var passwordConfirmationTextField: UITextField!
+    @IBOutlet private var passwordConfirmationIcon: UIImageView!
+    @IBOutlet private var createAccountButton: UIButton!
+    
+    private var userName = ""
+    private var password = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        userNameTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordConfirmationTextField.delegate = self
+
     }
-
-
+    
+    
 }
 
+// MARK: - Text Field Delegate
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        return true
+    }
+}
