f12173665ef5d5dc813a6f311cdd4dc387d02d25
Jhonny Bill Mena <jhonnybillm@gmail.com>
2021-03-28 10:45:15 -0400

    ADD - SwiftUI project

---
+++ WizardSchoolSignUpSwiftUI/WizardSchoolSignUpSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WizardSchoolSignUpSwiftUI
+//
+//  Created by Jhonny Bill on 3/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
