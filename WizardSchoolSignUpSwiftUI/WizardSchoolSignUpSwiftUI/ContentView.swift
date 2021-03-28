8e16c9a4f2bca104b3ac30574b1c610a6b0b254b
Jhonny Bill Mena <jhonnybillm@gmail.com>
2021-03-28 12:41:25 -0400

    ADD - initial Signup screen SwiftUI implementation

--- WizardSchoolSignUpSwiftUI/WizardSchoolSignUpSwiftUI/ContentView.swift
+++ WizardSchoolSignUpSwiftUI/WizardSchoolSignUpSwiftUI/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var passwordConfirmation: String = ""
+
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 24) {
+                textFields
+                createAccountButton
+                Spacer()
+            }
             .padding()
+            .navigationBarTitle("Wizard School Signup", displayMode: .inline)
+        }
+    }
+
+    private var textFields: some View {
+        VStack(spacing: 12) {
+            HStack {
+                Image(systemName: "person.circle")
+                TextField("Wizard name", text: $username)
+            }
+
+            HStack {
+                Image(systemName: "lock.circle")
+                TextField("Password", text: $password)
+            }
+
+            HStack {
+                Image(systemName: "lock.circle")
+                TextField("Password Confirmation", text: $passwordConfirmation)
+            }
+        }
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+    }
+
+    private var createAccountButton: some View {
+        Button(action: {}, label: {
+            HStack {
+                Spacer()
+                Text("Create Account")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            .padding()
+            .background(Color.green)
+            .cornerRadius(8)
+        })
     }
 }
 
